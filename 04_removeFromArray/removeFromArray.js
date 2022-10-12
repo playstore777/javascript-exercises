ff1d6a602f4d65493af70e804e94af0631465902
Play Store <ps494122@gmail.com>
2022-10-12 08:55:51 +0530

    Complete 4th JS exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...myargs) {
+    for (let iteration = 0; iteration < myargs.length; iteration++) {
+        if (array.includes(myargs[iteration])) {
+            let index = array.indexOf(myargs[iteration]);
+            let removeOnlyOne = 1;
+            if (index >= 0) {
+                array.splice(index, removeOnlyOne);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line

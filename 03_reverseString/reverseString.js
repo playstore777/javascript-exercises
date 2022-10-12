c80c0de70cb3d878b5d2c74901b527e234ff5160
Play Store <ps494122@gmail.com>
2022-10-12 08:35:32 +0530

    Complete 3rd JS exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // string is converted to a list, using SPLIT() it takes each character in the string as one element of the list.
+    // then after reversing we join it back as a string.
+    return string.split('').reverse().join("");
 };
 
 // Do not edit below this line

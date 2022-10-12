eaeb9d355aa5dbd3aa6144a3411e752e8619070e
Play Store <ps494122@gmail.com>
2022-10-12 08:31:07 +0530

    Complete 2nd JS exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatCount) {
+    if (repeatCount < 0) {
+        return "ERROR";
+    }
+    let repeatedString = "";
+    for (let iteration = 0; iteration < repeatCount; iteration++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line

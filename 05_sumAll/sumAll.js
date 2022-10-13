27badc1305a81b798eddef658608ad37b1b32cd8
Play Store <ps494122@gmail.com>
2022-10-13 08:05:53 +0530

    Complete 5th JS exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    let length = 0;
 
+    if (start < 0 || end < 0 || !Number.isSafeInteger(start) || !Number.isSafeInteger(end)) {
+        return "ERROR";
+    }
+
+    if (start > end) {
+        length = start;
+    } else {
+        length = end;
+    }
+
+    return ((length * (length + 1)) / 2); // Gauss formula
 };
 
 // Do not edit below this line

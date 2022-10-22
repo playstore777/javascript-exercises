540d95aafb8dc07f9ac05a02e83377d6adf66590
Play Store <ps494122@gmail.com>
2022-10-22 15:20:14 +0530

    Complete 8th JS exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(leftOp, rightOp) {
+	return leftOp + rightOp;
 };
 
-const subtract = function() {
-	
+const subtract = function(leftOp, rightOp) {
+    return leftOp - rightOp;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    return array.length > 0 ? array.reduce((prevSum, currVal) => prevSum + currVal) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    return array.length > 0 ? array.reduce((prevSum, currVal) => prevSum * currVal) : 0;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    let fact = 1;
+    for (let i = num; i > 0; i--) {
+        fact *= i;
+    }
+    return fact;
 };
 
 // Do not edit below this line

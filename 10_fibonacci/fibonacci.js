d5426b6f50e558c58addb99f823b3efbee62190d
Play Store <ps494122@gmail.com>
2022-10-23 08:24:03 +0530

    Complete 10th JS exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    index = parseInt(index);
+    let i = 1;
+    let fib = 1;
+    let buffer = fib;
+    for (let _ = 2; _ < index; _++) {
+        buffer = fib;
+        fib += i;
+        i = buffer;
+    }
+    return fib;
 };
 
 // Do not edit below this line

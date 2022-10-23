30134f7b2f2c9a3ca1749f1339a5157afd415d09
Play Store <ps494122@gmail.com>
2022-10-23 08:52:35 +0530

    Complete 12th JS exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = '';
+    let oldestPersonAge = -Infinity;
+    people.forEach(person => {
+        let DOB = person.yearOfBirth;
+        let DOD = person?.yearOfDeath ?? new Date().getFullYear(); // if DOD not available then current year!
+        let personAge = DOD - DOB;
 
+        if (personAge > oldestPersonAge) {
+            oldestPersonAge = personAge;
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line

43c3a903f4135c0eb756a05736f8dc823d2d6d0c
Play Store <ps494122@gmail.com>
2022-10-23 05:49:25 +0530

    Complete 9th JS exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let regEx = /[^a-zA-Z]/g;
+    str = str.replace(regEx, '');
+    let reveresedString = reverseString(str);
+    return str.toLowerCase() === reveresedString.toLowerCase();
 };
+
+const reverseString = (str) => {
+    return str.split('').reverse().join('');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
